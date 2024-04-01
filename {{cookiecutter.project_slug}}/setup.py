fdf36f53c3fbe3cc8e1e426c4abe9a3dcf45ec47
Richard <git.nordstrom@gmail.com>
2024-04-01 13:24:30 +0200

    additional formatting

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from setuptools import setup
+from setuptools import setup  # type: ignore
 
 # Setup.py included for backwards compatibility with editable installs
 setup()
