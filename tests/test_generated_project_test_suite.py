e5fa164ae65c722ef7407921c4f60d6ef47fb7c2
Richard Nordström <git.nordstrom@gmail.com>
2024-04-07 20:18:53 +0200

    impovments to tox

--- tests/test_generated_project_test_suite.py
+++ tests/test_generated_project_test_suite.py
@@ -1,11 +1,28 @@
 import subprocess
+import sys
 
 
-def test_tox(cookies):
+def test_tox_py(cookies):
+    pyenv = sys.version_info
     result = cookies.bake()
 
     p = subprocess.Popen(
-        ["tox"],
+        ["tox", "-e", f"py{pyenv.major}{pyenv.minor}"],
+        cwd=result.project_path,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+    )
+    _, err = p.communicate(timeout=100)
+
+    if p.returncode != 0:
+        raise RuntimeError(err)
+
+
+def test_tox_style(cookies):
+    result = cookies.bake()
+
+    p = subprocess.Popen(
+        ["tox", "-m", "style"],
         cwd=result.project_path,
         stderr=subprocess.PIPE,
         universal_newlines=True,
