126d322e6a427c08b4a2042fabb75ac37fbe8369
Richard Nordström <git.nordstrom@gmail.com>
2024-04-13 20:36:13 +0200

    improve generated project test pytest

--- tests/test_generated_project_test_suite.py
+++ tests/test_generated_project_test_suite.py
@@ -1,18 +1,20 @@
 import subprocess
 import sys
 
+TIMEOUT_DURATION_SEC = 180
+
 
 def test_tox_py(cookies):
     pyenv = sys.version_info
     result = cookies.bake()
 
     p = subprocess.Popen(
-        ["tox", "-e", f"py{pyenv.major}{pyenv.minor}"],
+        ["tox", "-e", f"py{pyenv.major}{pyenv.minor}", "-vv", "--", "-v"],
         cwd=result.project_path,
         stderr=subprocess.PIPE,
         universal_newlines=True,
     )
-    _, err = p.communicate(timeout=100)
+    _, err = p.communicate(timeout=TIMEOUT_DURATION_SEC)
 
     if p.returncode != 0:
         raise RuntimeError(err)
@@ -22,12 +24,12 @@ def test_tox_style(cookies):
     result = cookies.bake()
 
     p = subprocess.Popen(
-        ["tox", "-m", "style"],
+        ["tox", "-m", "style", "-vv", "--", "-v"],
         cwd=result.project_path,
         stderr=subprocess.PIPE,
         universal_newlines=True,
     )
-    _, err = p.communicate(timeout=100)
+    _, err = p.communicate(timeout=TIMEOUT_DURATION_SEC)
 
     if p.returncode != 0:
         raise RuntimeError(err)
