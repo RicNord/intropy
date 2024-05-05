ea7c1a0d82850f16848a89affccc8c38eb983cb9
Richard Nordström <git.nordstrom@gmail.com>
2024-05-05 20:50:24 +0200

    improve logging in cookiecutter proj

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -2,18 +2,26 @@ import argparse
 import logging
 from typing import Optional, Sequence
 
+{% if cookiecutter.project_type == 'Application' -%}
+from {{ cookiecutter.project_slug }} import setup_logging
+
+{% endif -%}
 logger = logging.getLogger(__name__)
 
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
     """Entry point"""
+{%- if cookiecutter.project_type == 'Application' %}
+
+    setup_logging()
+{%- endif %}
 
     parser = argparse.ArgumentParser(
         description="{{ cookiecutter.project_short_description }}",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
-    logger.info("Parse arguments")
+    logger.debug(f"Got {argv=}")
     # Add arguments
     _ = parser.parse_args(argv)
     return 0
