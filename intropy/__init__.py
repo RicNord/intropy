14970228eda20ee5c27fdb2bd7e37a1a98a871cf
Richard Nordström <git.nordstrom@gmail.com>
2024-05-05 20:02:34 +0200

    improve logging

--- intropy/__init__.py
+++ intropy/__init__.py
@@ -1,5 +1,53 @@
 __version__ = "0.4.0"
 
 import logging
+import logging.config
+import time
 
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+INTROPY_ROOT_LOGGER = logging.getLogger(__name__)
+
+
+class UTCFormatter(logging.Formatter):
+    converter = time.gmtime
+
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "simple": {"format": "%(levelname)s: %(message)s"},
+        "standard_utc": {
+            "()": UTCFormatter,
+            "format": "[%(levelname)s|%(name)s|L%(lineno)d] %(asctime)s: %(message)s",
+            "datefmt": "%Y-%m-%dT%H:%M:%S%z",
+        },
+    },
+    "handlers": {
+        "default": {
+            "level": "WARNING",
+            "formatter": "simple",
+            "class": "logging.StreamHandler",
+        },
+        "detailed": {
+            "level": "DEBUG",
+            "formatter": "standard_utc",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+        },
+    },
+    "loggers": {
+        "": {  # root logger
+            "handlers": ["default"],
+            "level": "DEBUG",
+        },
+        "__main__": {  # if __name__ == '__main__'
+            "handlers": ["detailed"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+    },
+}
+
+
+def setup_logging():
+    logging.config.dictConfig(LOGGING_CONFIG)
